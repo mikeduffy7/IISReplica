c753513524b7ae2854089c27678d0f48f9d38d54
mikeduffy7 <duffy.ma7@gmail.com>
2026-01-07 00:20:24 -0500

    add basic client

--- docs/week1-log.md
+++ docs/week1-log.md
@@ -1,9 +1,31 @@
 # Week 1 Progress Log
 ## Day 1
-- [ ] Read HTTP Made Really Easy
-- [ ] Read MDN HTTP Overview
-- [ ] Build HTTP client exercise
-- [ ] Answer architecture questions
+- [x] Read HTTP Made Really Easy
+- [x] Read MDN HTTP Overview
+- [x] Build HTTP client exercise
+- [x] Answer architecture questions
+
+- What are the 3 parts of an HTTP request? (Hint: request line, headers, body)
+    request line, body, header
+- What's in the request line? What information does it contain?
+    method, uri, http version
+- How does the server know when the request is complete? (This is critical for parsing!)
+    when the headers end or the body ends (depends on method)
+- What's the minimum valid HTTP/1.1 response? What must it contain?
+    status line (http version, statuscode, status reason)
+    blank link (CRLF)
+    optional body
+- Connection handling: What's the difference between HTTP/1.0 and HTTP/1.1 regarding connections? (Hint: Connection: close vs Keep-Alive)
+    http/1.0 => closes connection after each request (new tcp connection each request)
+    http/1.1 => default is keep-alive. content-length is important. connection will stay open until either side closes
+
+- HTTP runs on TCP (no `http://` in socket connections)
+- Request format: request line + headers + blank line + optional body
+- Response format: status line + headers + blank line + body
+- Headers end at `\r\n\r\n` (blank line)
+- Must handle reading in chunks/loops
+
+
 ## Day 2
 - [ ] Complete reading list
 - [ ] Design TCP listener architecture
--- src/IISReplica.Core/Program.cs
+++ src/IISReplica.Core/Program.cs
@@ -1,7 +1,71 @@
-﻿Console.WriteLine("IIS Replica - Starting Up...");
+﻿using System.Buffers.Text;
+using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("IIS Replica - Starting Up...");
 Console.WriteLine("Week 1: Building HTTP client excercises first");
 Console.WriteLine();
 
 // http client exercise goes here
+string host = "example.com";
+int port = 80;
+using TcpClient client = new TcpClient();
+Console.WriteLine($"Connecting to {host}:{port}...");
+
+await client.ConnectAsync(host, port);
+var stream = client.GetStream();
+var request = $"GET / HTTP/1.1\r\n" +
+            $"Host: {host}\r\n" +
+            $"\r\n";
+
+byte[] requestBytes = Encoding.UTF8.GetBytes(request);
+await stream.WriteAsync(requestBytes, 0, requestBytes.Length);
+Console.WriteLine("Connected to server!");
+
+byte[] buffer = new byte[4096];
+
+int byteRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+
+string response = Encoding.UTF8.GetString(buffer, 0, byteRead);
+
+string httpVersion = "";
+int statusCode = 0;
+string reasonPhrase = "";
+
+var lines = response.Split("\r\n");
+var statusLine = lines[0];
+var statusParts = statusLine.Split(" ");
+
+httpVersion = statusParts[0];
+statusCode = int.Parse(statusParts[1]);
+reasonPhrase = statusParts[2];
+
+Console.WriteLine($"Status: {statusCode}");
+Console.WriteLine($"Version: {httpVersion}");
+Console.WriteLine($"Reason: {reasonPhrase}");
+
+Console.WriteLine("\nHeaders:");
+for(var i = 1; i < lines.Length; i++)
+{
+    string line = lines[i];
+
+    if(string.IsNullOrEmpty(line))
+    {
+        Console.WriteLine("\n ---Body starts at line " + (i + 1) + "----" );
+        string body = string.Join("\r\n", lines.Skip(i + 1));
+        Console.WriteLine("\nBody: \r\n");
+        Console.WriteLine(body);
+
+        break;
+    }
+
+    var headers = line.Split(":", 2);
+    if(headers.Length == 2)
+    {
+        Console.WriteLine($"{headers[0].Trim()}: {headers[1].Trim()}");
+    }
+}
+
+
 Console.WriteLine("Press any key to exit...");
 Console.ReadKey();
